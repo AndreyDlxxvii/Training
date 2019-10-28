b04218d07d001e96e8a7d07f968e65f9b9d44c4d
AndreyDlxxvii <dlxxvii@gmail.com>
2019-10-28 20:51:25 +0300

    r

--- WinForms/WinForms/Program.cs
+++ WinForms/WinForms/Program.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
 
 namespace WinForms
 {
+
+    static class Game
+    {
+        static BufferedGraphicsContext context;
+        static public BufferedGraphics buffered;
+
+        static public int Width { get; set; }
+        static public int Height { get; set; }
+
+        static Game()
+        {
+
+        }
+
+        static public void Init (Form form)
+        {
+            Graphics gr;
+            context = BufferedGraphicsManager.Current;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            //hello worldфыв
+            Form form = new Form();
+            form.Width = 800;
+            form.Height = 600;
+            form.Show();
+            Application.Run(form);
+
         }
     }
 }
